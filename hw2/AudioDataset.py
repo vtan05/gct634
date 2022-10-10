d44c443aac5367655eb1a83757979359637f9ab0
LAPTOP-BA1MV7FC\Vanessa <vantan2555@gmail.com>
2022-10-10 23:33:19 +0900

    Starting Q2. TODO: augmentation

--- hw2/AudioDataset.py
+++ hw2/AudioDataset.py
@@ -1,14 +1,44 @@
-from torch.utils.data import Dataset, DataLoader
-import numpy as np
 import os
+import random
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+# from torchaudio_augmentations import (
+#     RandomResizedCrop,
+#     RandomApply,
+#     PolarityInversion,
+#     Noise,
+#     Gain,
+#     HighLowPass,
+#     Delay,
+#     PitchShift,
+#     Reverb,
+#     Compose,
+# )
 
 class AudioDataset(Dataset):
-    def __init__(self, paths, input_length, binary, id_to_path, split):
+    def __init__(self, paths, input_length, binary, id_to_path, split, is_augmentation=False):
         self.paths = paths
         self.input_length = input_length
         self.binary = binary
         self.id_to_path = id_to_path
         self.split = split
+        self.is_augmentation = is_augmentation
+        if is_augmentation:
+            self._get_augmentations()
+
+    # def _get_augmentations(self):
+    #     transforms = [
+    #         RandomResizedCrop(n_samples=self.num_samples),
+    #         RandomApply([PolarityInversion()], p=0.8),
+    #         RandomApply([Noise(min_snr=0.3, max_snr=0.5)], p=0.3),
+    #         RandomApply([Gain()], p=0.2),
+    #         RandomApply([HighLowPass(sample_rate=16000)], p=0.8),
+    #         RandomApply([Delay(sample_rate=16000)], p=0.5),
+    #         RandomApply([PitchShift(n_samples=self.num_samples, sample_rate=16000)], p=0.4),
+    #         RandomApply([Reverb(sample_rate=16000)], p=0.3),
+    #     ]
+    #     self.augmentation = Compose(transforms=transforms)
 
     def __getitem__(self, index):
         item = self.binary.iloc[index]
